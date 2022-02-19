2d0df450e2bf3553c568688348df3e14a8ed7466
Vicente Viera <plantedcoot706@gmail.com>
2022-02-19 14:03:53 -0600

    Structure of page and form declared

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from "@emotion/styled"
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3sease;
+
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+
+    }
+`
+
+
+const Formulario = () => {
+  return (
+    <form>
+        <InputSubmit 
+            type="submit" 
+            value="Cotizar"
+        />
+    </form>
+  )
+}
+
+export default Formulario
