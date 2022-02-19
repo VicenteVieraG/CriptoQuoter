6ea43530c8518cb8de4da7303cd8c91da77b8f8b
Vicente Viera <plantedcoot706@gmail.com>
2022-02-19 15:59:49 -0600

    Data folder created, and SelectMonedas structure done

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import styled from "@emotion/styled"
+import useSelectMonedas from "../hooks/useSelectMonedas"
+import {monedas} from "../data/monedas.jsx"
 
 const InputSubmit = styled.input`
-    background-color: #9497FF;
-    border: none;
-    width: 100%;
-    padding: 10px;
-    color: #FFF;
-    font-weight: 700;
-    text-transform: uppercase;
-    font-size: 20px;
-    border-radius: 5px;
-    transition: background-color .3sease;
+	background-color: #9497FF;
+	border: none;
+	width: 100%;
+	padding: 10px;
+	color: #FFF;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 20px;
+	border-radius: 5px;
+	transition: background-color .3s ease;
 
-    &:hover {
-        background-color: #7A7DFE;
-        cursor: pointer;
-
-    }
+	&:hover {
+		background-color: #7A7DFE;
+		cursor: pointer;
+	}
 `
 
 
 const Formulario = () => {
-  return (
-    <form>
-        <InputSubmit 
-            type="submit" 
-            value="Cotizar"
-        />
-    </form>
-  )
+	const [SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+
+
+	return (
+		<form>
+			<SelectMonedas/>
+
+			<InputSubmit 
+				type="submit" 
+				value="Cotizar"
+			/>
+		</form>
+	)
 }
 
 export default Formulario
